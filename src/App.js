63bde54d8c078b9a71d5e42d9a0356448b8dc6ce
Corrinne <musselmammary@gmail.com>
2023-04-21 17:29:14 +0800

    feat: learnin learnin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Test nia ho</p>
+        <h3>What I want to learn</h3>
+
+        <ul>
+          <li>
+            Data fetching, promises???? The slab roadmap stuff (but perhaps best
+            when applied to real scenarios)
+          </li>
+          <li>What is Async vs sync? and await </li>
+          <li>What is CI/CD </li>
+          <li>What even is node.js </li>
+          <li>What is a Cron job </li>
+          <li>What is Production vs Dev mode in js, just now you mentioned </li>
+          <li>What other ways are there to deploy to production? </li>
+        </ul>
+
         <img src="/sad-cat.gif" alt="sad-cat" />
       </header>
     </div>
