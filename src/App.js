14a26043b26da44b13099132de107a270fdbd44e
Corrinne <musselmammary@gmail.com>
2023-04-21 15:58:46 +0800

    added sad cat

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Test nia </p>
-        <p>kkkkkkkkkkkk</p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Test nia ho</p>
+        <img src="/sad-cat.gif" alt="sad-cat" />
       </header>
     </div>
   );
